e32153bf3279edd76b500257d365aa3c0dac5625
JINALI2504 <166807280+JINALI2504@users.noreply.github.com>
2025-06-18 10:36:27 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountID = 12345
+let accountName = "jinali"
+var accountPassword= "1234"
+
+console.log([accountID, accountName, accountPassword]);
+console.table([accountID, accountName, accountPassword]);
