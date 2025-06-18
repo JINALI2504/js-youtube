de074124ed4098a8bcae1dc914526eb09a78274b
JINALI2504 <166807280+JINALI2504@users.noreply.github.com>
2025-06-18 10:04:25 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as new version
+let name= "jinali"
+let age= 20
+let isLoggedIn = false
+let state;
+// bigint
+// string==> ""
+// boolean ==> true/false
+// null==> standalone value
+// undefined==> no value is assigned
+// symbol==> unique
+
+// object
+console.log(typeof null);
+console.log(typeof undefined);
